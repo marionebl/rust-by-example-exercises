245eb1758c8c104821928c798720060fc68fe7bf
Mario Nebl <hello@mario-nebl.de>
2017-11-30 17:43:53 +0100

    feat: implement display list

---
+++ 1-2-2-1-display-list.rs
@@ -0,0 +1,26 @@
+use std::fmt;
+
+struct List(Vec<i32>);
+
+impl fmt::Display for List {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    let vec = &self.0;
+
+    write!(f, "[")?;
+
+    for (count, v) in vec.iter().enumerate() {
+      if count != 0 { write!(f, ", ")?; }
+      let result = write!(f, "{}: {}", count, v);
+      if result.is_err() {
+        return result;
+      }
+    }
+
+    write!(f, "]")
+  }
+}
+
+fn main() {
+  let v = List(vec![1, 2, 3]);
+  println!("{}", v);
+}
