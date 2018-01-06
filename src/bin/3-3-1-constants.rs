a77114aee92270b8dace881817e9e31f56200d3e
Mario Nebl <hello@mario-nebl.de>
2018-01-06 21:58:18 +0100

    Add constants tutorial

---
+++ src/bin/3-3-1-constants.rs
@@ -0,0 +1,20 @@
+// Globals are declared in the root scope
+static LANGUAGE: &'static str = "Rust";
+const THRESHOLD: i32 = 10;
+
+fn is_big(n: i32) -> bool {
+    // Access constant in some function
+    n > THRESHOLD
+}
+
+fn main() {
+    let n = 16;
+
+    // Access constant in the main thread
+    println!("This is {}", LANGUAGE);
+    println!("The threshold is {}", THRESHOLD);
+    println!("{} is {}", n, if is_big(n) { "big" } else { "small" });
+
+    // https://doc.rust-lang.org/error-index.html#E0070
+    // THRESHOLD = 5;
+}
