f80b0994a40d87819564773a081e01c9884170c1
Mario Nebl <hello@mario-nebl.de>
2018-01-13 11:19:32 +0100

    Add example for From/Into traits

---
+++ src/bin/6-1-from-into.rs
@@ -0,0 +1,21 @@
+use std::convert::From;
+
+#[derive(Debug)]
+struct Number {
+    value: i32,
+}
+
+impl From<i32> for Number {
+    fn from(value: i32) -> Self {
+        Number { value }
+    }
+}
+
+fn main() {
+    let num = Number::from(30);
+    println!("My number is {:?}", num);
+
+    let int = 5;
+    let num: Number = int.into();
+    println!("My second number is {:?}", num);
+}
