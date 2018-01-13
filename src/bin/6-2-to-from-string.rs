69f4c285cc8970af0a07a7e4c512d6a2f67ff818
Mario Nebl <hello@mario-nebl.de>
2018-01-13 11:58:06 +0100

    Refactor to impl std::fmt::Display

--- src/bin/6-2-to-from-string.rs
+++ src/bin/6-2-to-from-string.rs
@@ -1,16 +1,38 @@
-use std::string::ToString;
+use std::str::FromStr;
+use std::num::ParseIntError;
+use std::fmt;
 
+#[derive(Debug)]
 struct Circle {
     radius: i32
 }
 
-impl ToString for Circle {
-    fn to_string(&self) -> String {
-        format!("Circle of radius {:?}", self.radius)
+impl fmt::Display for Circle {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "(r: {})", self.radius)
+    }
+}
+ 
+impl FromStr for Circle {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let result = i32::from_str(s);
+
+        match result {
+            Ok(radius) => Ok(Circle { radius }),
+            Err(err) => Err(err)
+        }
     }
 }
 
 fn main() {
     let circle = Circle { radius: 6 };
     println!("{}", circle.to_string());
+
+    let circle = Circle::from_str("foo");
+    println!("invalid circle: {:?}", circle); // TODO: How to implement Display for this?
+
+    let circle = Circle::from_str("1").unwrap();
+    println!("{}", circle);
 }
