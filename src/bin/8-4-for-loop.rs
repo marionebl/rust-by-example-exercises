d83f65772087617f581e5b459be32633fcf0add3
Mario Nebl <hello@mario-nebl.de>
2018-01-19 13:09:26 +0100

    Start for/iterator example

---
+++ src/bin/8-4-for-loop.rs
@@ -0,0 +1,14 @@
+fn main() {
+    // .. is end-exclusive
+    for n in 1..101 {
+        if n % 15 == 0 {
+            println!("fizzbuzz");
+        } else if n % 3 == 0 {
+            println!("fizz");
+        } else if n % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", n);
+        }
+    }
+}
