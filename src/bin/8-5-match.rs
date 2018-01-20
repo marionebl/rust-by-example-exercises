832c1c0132b8d2215d7b4194dd15e2d4e4420e96
Mario Nebl <hello@mario-nebl.de>
2018-01-20 22:12:35 +0100

    Expand example with user input

--- src/bin/8-5-match.rs
+++ src/bin/8-5-match.rs
@@ -1,21 +1,58 @@
+use std::io;
+use std::num;
+
 fn main() {
-    let number = 13;
+    loop {
+        println!("Enter a number:");
 
-    println!("Tell me about {}", number);
+        let number = match get_input() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Try again.");
+                continue;
+            },
+        };
 
-    match number {
-        1 => println!("One!"),
-        2 | 3 | 5 | 7 | 11 => println!("This is a prime"),
-        13...19 => println!("A teen"),
-        _ => println!("Ain't special")
+        println!("Tell me about {}", number);
+
+        match number {
+            11 | 12 => println!("This is 11 | 12"),
+            13...19 => println!("This is 13...19"),
+            num if is_prime(num) => println!("Is a prime"),
+            _ => println!("Ain't special")
+        }
     }
+}
 
-    let boolean = true;
+enum InputError {
+    Io(io::Error),
+    Parse(num::ParseIntError),
+}
 
-    let binary = match boolean {
-        false => 0,
-        true => 1
-    };
+fn get_input() -> Result<i32, InputError> {
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(i) => Ok(i),
+        Err(err) => Err(InputError::Io(err)),
+    }?;
+    match input.trim().parse::<i32>() {
+        Ok(num) => Ok(num),
+        Err(err) => Err(InputError::Parse(err)),
+    }
+}
 
-    println!("{} -> {}", boolean, binary);
+fn is_prime(num: i32) -> bool {
+    match num {
+        0 | 1 => false,
+        2 => true,
+        n => {
+            for i in 2..(n as f64).sqrt() as i64+1 {
+                if n % (i as i32) == 0 {
+                    return false
+                }
+            }
+
+            true
+        }
+    }
 }
