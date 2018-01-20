c43800d9069455e2f3ba34b6f47f7c6dd448646a
Mario Nebl <hello@mario-nebl.de>
2018-01-20 21:34:31 +0100

    Add iter_mut example

--- src/bin/8-4-for-loop.rs
+++ src/bin/8-4-for-loop.rs
@@ -12,15 +12,36 @@ fn main() {
         }
     }
 
-    let names = vec!["Bob", "Frank", "Ferris"];
+    let mut names = vec!["Bob", "Frank", "Ferris"];
+
     iter(&names);
+    iter_mut(&mut names);
+    into_iter(names); // consumes names    
 }
 
 fn iter(names: &Vec<&str>) {
     for name in names.iter() {
         match *name {
             "Ferris" => println!("There is a rustacean among us!"),
-            _ => println!("Hello {}", name)
+            _ => println!("Hello {}", name),
+        }
+    }
+}
+
+fn iter_mut(names: &mut Vec<&str>) {
+    for name in names.iter_mut() {
+        match name {
+            &mut "Ferris" => println!("There is a rustacean among us!"),
+            _ => println!("Hello {}", name),
+        }
+    }
+}
+
+fn into_iter(names: Vec<&str>) {
+    for name in names.into_iter() { // starting from here name is moved
+        match name {
+            "Ferris" => println!("There is a rustacean among us!"),
+            _ => println!("Hello {}", name),
         }
     }
 }
