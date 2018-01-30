56d8a2e62cc2dec9c85a072acfe70258babe2c93
Mario Nebl <hello@mario-nebl.de>
2018-01-30 23:49:36 +0100

    Add more if-let cases

--- src/bin/8-6-if-let.rs
+++ src/bin/8-6-if-let.rs
@@ -1,25 +1,54 @@
 use std::io;
 
 fn main() {
-    let number = get_input();
+    println!("number:");
+    let number = get_number();
 
-    let letter: Option<i32> = None;
-    let emoticon: Option<i32> = None;
+    println!("letter:");
+    let letter = get_letter();
+
+    println!("number={:?}", number);
+    println!("letter={:?}", letter);
 
     if let Some(i) = number {
-        println!("Matched {:?}!", i);
+        println!("Matched number {:?}!", i);
     } 
+
+    let i_like_letters = false;
+
+    if let Some(i) = letter {
+        println!("Matched letter {:?}!", i);
+    } else if i_like_letters {
+        println!("Didn't match a number. Let's go with a letter!");
+    } else {
+        println!("I don't like letters. Let's go with an emoticon :)!");
+    }
 }
 
-fn get_input() -> Option<i32> {
+fn get_number() -> Option<i32> {
     let mut result = String::new();
 
     if let Err(_) = io::stdin().read_line(&mut result) {
         return None;
     }
 
-    match result.parse() {
+    println!("result {:?}", result.trim());
+
+    match result.trim().parse::<i32>() {
         Ok(i) => Some(i),
-        Err(_) => None,
+        Err(_) => None
+    }
+}
+
+fn get_letter() -> Option<String> {
+    let mut result = String::new();
+
+    if let Err(_) = io::stdin().read_line(&mut result) {
+        return None;
+    }
+
+    match result.trim().parse::<i32>() {
+        Ok(_) => None,
+        Err(_) => Some(result)
     }
 }
