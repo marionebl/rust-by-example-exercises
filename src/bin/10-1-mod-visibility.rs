98d7830585f4a78cfa8bfa5be258c08d81181cdb
Mario Nebl <hello@mario-nebl.de>
2018-02-16 18:25:28 +0100

    Show how to scope visibility

--- src/bin/10-1-mod-visibility.rs
+++ src/bin/10-1-mod-visibility.rs
@@ -4,6 +4,7 @@ fn main() {
 
     my_mod::indirect_access();
     my_mod::nested::function();
+    my_mod::call_public_function_in_my_mod();
 
     // error[E0603]: function `private_function` is private
     // my_mod::private_function();
@@ -27,9 +28,29 @@ mod my_mod {
         private_function();
     }
 
+    pub fn call_public_function_in_my_mod() {
+        print!("called `my_mod::call_public_function_in_my_mod()`, that\n> ");
+        nested::public_function_in_my_mod();
+        print!("> ");
+        nested::public_function_in_super_mod();
+    }
+
     pub mod nested {
         pub fn function() {
             println!("called `my_mod::nested::function()`");
+        }
+
+        pub(in my_mod) fn public_function_in_my_mod() {
+            print!("called `my_mod::nested::public_function_in_my_mod()`, that\n > ");
+            public_function_in_nested();
+        }
+
+        pub(self) fn public_function_in_nested() {
+            println!("called `my_mod::nested::public_function_in_nested()`");
+        }
+
+        pub(super) fn public_function_in_super_mod() {
+            println!("called `my_mod::nested::public_function_in_super_mod()`");
         }
     }
 }
