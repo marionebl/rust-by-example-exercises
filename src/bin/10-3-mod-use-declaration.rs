b72ad07c16984c6a05005434f2a63009a755e993
Mario Nebl <hello@mario-nebl.de>
2018-02-17 15:56:49 +0100

    Demonstrate block scoping of use

--- src/bin/10-3-mod-use-declaration.rs
+++ src/bin/10-3-mod-use-declaration.rs
@@ -2,6 +2,21 @@ use deeply::nested::function as other;
 
 fn main() {
     other();
+
+    println!("Entering block");
+
+    {
+        use deeply::nested::function;
+        function();
+
+        println!("Leaving block");
+    }
+
+    function();
+}
+
+fn function() {
+    println!("called `function()`");
 }
 
 mod deeply {
