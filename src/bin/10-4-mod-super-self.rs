fd89160e6148fe702f2cf32d14d9ed9f81300e3c
Mario Nebl <hello@mario-nebl.de>
2018-02-17 16:00:30 +0100

    Show basic self usage

--- src/bin/10-4-mod-super-self.rs
+++ src/bin/10-4-mod-super-self.rs
@@ -3,7 +3,14 @@ fn main() {
 }
 
 mod my {
+    fn function() {
+        println!("called `my::function()`");
+    }
+
     pub fn indirect_call() {
         print!("called `my::indirect_call()`, that\n> ");
+
+        self::function();
+        function();
     }
 }
