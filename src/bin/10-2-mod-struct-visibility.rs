e9d0b1bfe79ab91338fc9a5b57f155fe379df475
Mario Nebl <hello@mario-nebl.de>
2018-02-17 15:52:31 +0100

    Complete struct visibility example

--- src/bin/10-2-mod-struct-visibility.rs
+++ src/bin/10-2-mod-struct-visibility.rs
@@ -1,10 +1,34 @@
 mod box_mod {
+    use std::fmt;
+
     pub struct Open<T> {
         pub contents: T,
+    }
+
+    pub struct Closed<T> {
+        contents: T,
+    }
+
+    impl<T: fmt::Debug> Closed<T> {
+        pub fn new(contents: T) -> Closed<T> {
+            Closed { contents }
+        }
+        pub fn peek(&self) -> &T {
+            &self.contents
+        }
     }
 }
 
 fn main() {
     let open_box = box_mod::Open { contents: "public information" };
     println!("The open box contains: {}", open_box.contents);
+
+    // error[E0451]: field `contents` of struct `box_mod::Closed` is private
+    // let closed_box = box_mod::Closed { contents: "classified information" };
+
+    let _closed_box = box_mod::Closed::new("classified information");
+
+    // error[E0616]: field `contents` of struct `box_mod::Closed` is private
+    // println!("The closed box contains: {}", _closed_box.contents);
+    println!("Peek into closed box: {}", _closed_box.peek());
 }
