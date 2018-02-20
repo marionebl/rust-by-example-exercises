c0333daebb47f3fc1048df409d89ac57133d4fc3
Mario Nebl <hello@mario-nebl.de>
2018-02-20 10:26:44 +0100

    Expand example by boxed i32

--- src/bin/13-2-generic-functions.rs
+++ src/bin/13-2-generic-functions.rs
@@ -1,6 +1,7 @@
 fn main() {
     reg_fn(S(A));
     gen_spec_t(SGen(A));
+    gen_spec_i32(SGen(6));
 }
 
 struct A; // concrete
@@ -9,3 +10,4 @@ struct SGen<T>(T); // generic
 
 fn reg_fn(_s: S) {} // concrete
 fn gen_spec_t(_s: SGen<A>) {} // concrete using a generic struct
+fn gen_spec_i32(_s: SGen<i32>) {} // concrete using a generic struct
