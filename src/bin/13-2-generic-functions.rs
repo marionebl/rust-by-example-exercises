074bc1eb44d14a7c9b3c7c382f58a52792ccf648
Mario Nebl <hello@mario-nebl.de>
2018-02-20 10:25:37 +0100

    Show concrete usage of generic struct

--- src/bin/13-2-generic-functions.rs
+++ src/bin/13-2-generic-functions.rs
@@ -1,8 +1,11 @@
 fn main() {
     reg_fn(S(A));
+    gen_spec_t(SGen(A));
 }
 
-struct A;
-struct S(A);
+struct A; // concrete
+struct S(A); // concrete
+struct SGen<T>(T); // generic
 
-fn reg_fn(_s: S) {}
+fn reg_fn(_s: S) {} // concrete
+fn gen_spec_t(_s: SGen<A>) {} // concrete using a generic struct
