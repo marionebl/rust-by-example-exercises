21dcb5aaafd26808035ac727431d94284b833ebb
Mario Nebl <hello@mario-nebl.de>
2018-02-20 10:28:00 +0100

    Introduce type params for functions and turbo-fish syntax

--- src/bin/13-2-generic-functions.rs
+++ src/bin/13-2-generic-functions.rs
@@ -2,6 +2,10 @@ fn main() {
     reg_fn(S(A));
     gen_spec_t(SGen(A));
     gen_spec_i32(SGen(6));
+
+    generic::<char>(SGen('a'));
+
+    generic(SGen('c'));
 }
 
 struct A; // concrete
@@ -11,3 +15,4 @@ struct SGen<T>(T); // generic
 fn reg_fn(_s: S) {} // concrete
 fn gen_spec_t(_s: SGen<A>) {} // concrete using a generic struct
 fn gen_spec_i32(_s: SGen<i32>) {} // concrete using a generic struct
+fn generic<T>(_s: SGen<T>) {} // generic
