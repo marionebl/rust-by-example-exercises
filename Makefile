90bb2aeb9b9d5333a7e59e287f07a3285b267704
Mario Nebl <hello@mario-nebl.de>
2018-02-20 10:03:36 +0100

    Add examples for crate attributes

--- Makefile
+++ Makefile
@@ -4,6 +4,10 @@
 11-crates-executable:
 	rustc src/11-crates-executable.rs --extern rary=lib11_crates_library.rlib && ./11-crates-executable
 
+12-attributes-crates:
+	rustc src/12-attributes-crates.rs
+
 clean:
 	rm -rf 11-crates-executable
-	rm -rf lib11_crates_library
+	rm -rf lib11_crates_library	
+	rm -rf lib12_attributes_crates
---
+++ src/12-attributes-crates.rs
@@ -0,0 +1,15 @@
+#![crate_type = "lib"]
+#![crate_name = "rary"]
+
+pub fn public_function() {
+    println!("called rary's `public_function`");
+}
+
+pub fn indirect_access() {
+    print!("called rary's `indirect_access()`, that\n> ");
+    private_function();
+}
+
+fn private_function() {
+    println!("called rary's `private_function()`");
+}
