fdb3e611bbfd7d606f5963c4749a8ef43ef5ed0a
Mario Nebl <hello@mario-nebl.de>
2018-02-21 20:52:32 +0100

    Show usage of user-defined traits for bounds

--- src/bin/13-5-generic-bounds.rs
+++ src/bin/13-5-generic-bounds.rs
@@ -1,7 +1,16 @@
 use std::fmt::Debug;
 
+trait HasArea {
+    fn area(&self) -> f64;
+}
+
+impl HasArea for Rectangle {
+    fn area(&self) -> f64 { self.length * self.height }
+}
+
 #[derive(Debug)]
 struct Rectangle { length: f64, height: f64 }
+#[allow(dead_code)]
 struct Triangle { length: f64, height: f64 }
 
 fn main() {
@@ -9,11 +18,16 @@ fn main() {
     let _triangle = Triangle { length: 3.0, height: 4.0 };
 
     print_debug(&rectangle);
+    println!("Area: {}", area(&rectangle));
 
     // error[E0277]: the trait bound `Triangle: std::fmt::Debug` is not satisfied
     // print_debug(&_triangle);
+    // error[E0277]: the trait bound `Triangle: HasArea` is not satisfied
+    // println!("Area: {}", area(&_triangle));
 }
 
 fn print_debug<T: Debug>(t: &T) {
     println!("{:?}", t);
 }
+
+fn area<T: HasArea>(t: &T) -> f64 { t.area() }
