ce31743931b8628436119ffa0503411238017d14
Mario Nebl <hello@mario-nebl.de>
2018-02-25 22:36:09 +0100

    Finalize multiple bounds demo

--- src/bin/13-5-2-generic-bounds-multiple.rs
+++ src/bin/13-5-2-generic-bounds-multiple.rs
@@ -6,9 +6,16 @@ fn main() {
     let vec = vec![1, 2, 3];
 
     compare_prints(&string);
+
+    compare_types(&array, &vec);
 }
 
 fn compare_prints<T: Debug + Display>(t: &T) {
     println!("Debug: {:?}", t);
     println!("Display: {}", t);
+}
+
+fn compare_types<T: Debug, U: Debug>(t: &T, u: &U) {
+    println!("t: {:?}", t);
+    println!("u: {:?}", u);
 }
